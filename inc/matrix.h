dca7993f4f4aee7fccb77a1dc627745f07c944c3
Michael Jansky <michael.jansky@stud-mail.uni-wuerzburg.de>
2020-01-21 21:34:25 +0100

    some changes

--- inc/matrix.h
+++ inc/matrix.h
@@ -37,10 +37,25 @@ private:
 public:
     typedef Row<COLS, T>* iterator;
     typedef const Row<COLS, T>* const_iterator;
+    
     Matrix(){
-        for(auto row: *this){
+        int count = 0;
+        for(auto& row: *this){
             for(auto &e: row){
-                e = T(0);
+                if(count/COLS == count%COLS){
+                    e = T(1);
+                }else{
+                    e = T(0);
+                }
+                count++;
+            }
+        }
+    }
+
+    Matrix(Matrix& mat){
+        for(int row = 0; row < ROWS; row++){
+            for(int col = 0; col < COLS; col++){
+                (*this)[row][col] = mat[row][col];
             }
         }
     }
@@ -52,31 +67,62 @@ public:
 
     }
 
+   template <typename... Args>
+      Matrix(Args... args){
+           T a[] = {args...};
+           int count = 0;
+           while(count < ROWS * COLS){
+               for(auto e : a){
+                   if(count < ROWS * COLS){
+                       (*this)[count/COLS][count%COLS] = e;
+                       count++;
+                   }else{
+                       break;
+                   }
+               }
+           }
+       }
+
    Row<COLS, T>& operator[](int row){
         return m[row];
     };
     iterator begin(){return &m[0];}
     iterator end(){return &m[ROWS];}
+
     void print(){
+        int counter = 0;
         for(auto row: m){
-            for(auto e: row){
-                std::cout << e << " ";
+            if(counter == 0){
+                std::cout << "\n[";
+            }else{
+                std::cout << " ";
             }
-            std::cout << std::endl;
+            for(auto e: row){
+                counter++;
+                std::cout << e;
+                if(counter == ROWS * COLS){
+                    std::cout << "]";
+                }else{
+                    std::cout << "\t";
+                }
+            }
+            std::cout <<  std::endl;
         }
     }
 
 
     // Basic Calculation
-    Matrix operator+(Matrix& other){
-        T res[ROWS][COLS];
+Matrix operator+(Matrix other){
+    T res[ROWS][COLS];
         for(int row = 0; row < ROWS; row++){
             for(int col = 0; col < COLS; col++){
                 res[row][col] = other[row][col] + (*this)[row][col];
             }
         }
-        return Matrix(res);
+        Matrix result(res);
+        return result;
     }
+
     Matrix operator*(T other){
         T res[ROWS][COLS];
         for(int row = 0; row < ROWS; row++){
@@ -84,11 +130,13 @@ public:
                 res[row][col] = other * (*this)[row][col];
             }
         }
-        return Matrix(res);
+        Matrix result(res);
+        return result;
    }
 
+   /*
    template<int OTHER_COLS>
-   Matrix<ROWS, OTHER_COLS, T> operator*(Matrix<COLS, OTHER_COLS, T>& other){
+   Matrix<ROWS, OTHER_COLS, T> operator*(Matrix<COLS, OTHER_COLS, T> other){
        T result[ROWS][OTHER_COLS];
        for(int row = 0; row < ROWS; row++){
            for(int col = 0; col < OTHER_COLS; col++){
@@ -102,11 +150,19 @@ public:
        Matrix<ROWS, OTHER_COLS, T> m(result);
        return m;
    }
-
-   friend Matrix operator*(T scalar, Matrix& mat){
-       Matrix result = mat * scalar;
+*/
+   Matrix operator-(Matrix other){
+       Matrix result;
+       result = (*this) + other;
        return result;
    }
+
+   friend Matrix operator*(T scalar, Matrix mat){
+       Matrix result;      
+       result = mat * scalar;
+       return result;
+   }
+
         
    friend std::ostream & operator << (std::ostream & out, Matrix mat){
         out << "[";
--- main.cpp
+++ main.cpp
@@ -3,24 +3,10 @@ using namespace std;
 #include "matrix.h"
 
 int main(){
-    float a[2][2] = {{1,2},{3,4}};
-    Matrix<2,2,float> m(a);
-    Matrix<2,2,float> n = m;
-    cout << "n = m = " << n << endl;
-    m[0][0] = 11;
-    cout << "changed first element of m. n should remain the same: " << n << " but m is different: " << m << endl;
-    n[0][1] = 12;
-    cout << "changed first element of n. m should remain the same: " << m << " but m is different: " << n << endl;
-    
-    
-    float b[2][3] = {{1,2,3},{4,5,6}};
-    float c[3][2] = {{1,2},{3,4},{5,6}};
-    
-    Matrix<2,3,float> K(b);
-    Matrix<3,2,float> L(c);
-
-    m = K*L;
-    K.print();
-    L.print();
-    m.print();
+    Matrix<3,3,int> A;
+    Matrix<3,3,int> B;
+    Matrix<3,3,int> res;
+    res = A * B + B - B;
+    res = A * 3 * 3;
+    res.print();
 }
