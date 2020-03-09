80ee2f7ef34ab492bc10cb7b2dd29b7b02fcfb37
Michael Jansky <michael.jansky@stud-mail.uni-wuerzburg.de>
2020-03-09 12:01:32 +0100

    Outsourced the forward and backward substitution

--- inc/gauss-elimination.h
+++ inc/gauss-elimination.h
@@ -1,32 +1,8 @@
 #include "matrix.h"
+#include "triangular-matrix-substitution.h"
 
 namespace GAUSS{
 
-    template<int N, class T>
-        Matrix<N,1,T> backSub(Matrix<N,N,T> R, Matrix<N,1,T> b){
-            Matrix<N,1,T> x;
-            for(int i = N-1; i >= 0; i--){
-                T sum = T(0);
-                for(int j = i+1; j < N; j++){
-                    sum += R[i][j] * x[j][0];
-                }
-                x[i][0] = (b[i][0] - sum)/R[i][i];
-            }
-            return x;
-        }
-
-    template<int N, class T>
-        Matrix<N,1,T> forSub(Matrix<N,N,T> R, Matrix<N,1,T> b){
-            Matrix<N,1,T> x;
-            for(int i = 0; i < N; i++){
-                T sum = T(0);
-                for(int j = 0; j < i; j++){
-                    sum += R[i][j] * x[j][0];
-                }
-                x[i][0] = (b[i][0] - sum)/R[i][i];
-            }
-            return x;
-        }
 
     template<int N, class T>
         bool gaussStep(Matrix<N, N, T>& A, Matrix<N, 1, T>& b, Matrix<N, N, T>& L_j, 
